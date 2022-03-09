ed5170a79d675bde8e37ccb68cbc94b25ddbf4cd
Swift bb-mp <>
2022-03-09 17:01:31 +0300

    first commit.

---
+++ Hatirlatici/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hatirlatici
+//
+//  Created by Swift on 9.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
