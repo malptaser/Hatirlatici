95213cb8096fe8c036f75849f00d4a12b448d70c
Swift bb-mp <>
2022-03-11 18:47:32 +0300

    ReminderViewController elemanları bağlandı.

--- Hatirlatici/Controllers/AddReminderViewController.swift
+++ Hatirlatici/Controllers/AddReminderViewController.swift
@@ -9,5 +9,30 @@ import UIKit
 
 class AddReminderViewController: UITableViewController {
 
+  
+
+    // MARK: - UI Elements
+    @IBOutlet weak var titleTexField: UITextField!
+    @IBOutlet weak var remindMeSwitch: UISwitch!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var addBarButton: UIBarButtonItem!
+        
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+    }
+    
+    // MARK: - Actions
+
+    @IBAction func datePickerValueChanged(_ sender: Any) {
+    }
+    
+    @IBAction func remindMeSwitchValueChanged(_ sender: Any) {
+    }
+}
+
+
+
+
